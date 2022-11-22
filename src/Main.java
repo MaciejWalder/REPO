eef07ad3be8fdbd670b9bdcd8d03df817b307fc6
Maciej <walder_m@wp.pl>
2022-11-22 20:15:41 +0100

    Przedstawienie się

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         String name="Maciej";
-          int age=39;
+          int age=25;
           System.out.println(("Czesc mam na imie "+name +" i mam "+ age +" lat"));
 
     }
