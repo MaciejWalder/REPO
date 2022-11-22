2ae9c290bc516ef1818d68cf0104b3d60294fcb9
Maciej <walder_m@wp.pl>
2022-11-22 20:31:00 +0100

    Dodanie zapytań o imie i wiek.

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,12 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        String name="Maciej";
-          int age=25;
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("Podaj swoje imie");
+        String name=scanner.nextLine();
+        System.out.println("Podaj swoj wiek");
+          int age=scanner.nextInt();
           System.out.println(("Czesc mam na imie "+name +" i mam "+ age +" lat"));
 
     }
